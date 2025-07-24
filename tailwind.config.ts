84f8064ac879d5c77a11693bf04c556761f0d88b
Jack Malcom <j@ckmal.com>
2025-07-24 14:33:59 -0500

    yoink the solace color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,23 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        solace: {
+          white: "#fff",
+          black: "#101010",
+          primary: "#1d4339",
+          "primary-focused": "#285e50",
+          "primary-selected": "#347866",
+          "accent-gold": "#e9cc95",
+          gold: "#d7a13b",
+          "neutral-dark-grey": "#5a5a5a",
+          "neutral-grey": "#9a9a9a",
+          "neutral-light-grey": "#e9e9e9",
+          opal: "#d4e2dd",
+          "green-100": "#f4f8f7",
+          "accent-mid": "#3f937c",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
